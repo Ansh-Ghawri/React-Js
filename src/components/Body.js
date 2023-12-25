aa2d47199de7fad6833cc7d4fa42c04ae05654b1
Ansh-Ghawri <ghawri.ansh@gmail.com>
2023-12-25 23:40:49 +0530

    Search Functionality + Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,82 @@
 import RestaurantCard from "./RestaurantCard";
-import {restList} from "../utils/mockdata"
-import { useState } from "react";
+import { restList } from "../utils/mockdata";
+import { useState, useEffect } from "react";
+import { Shimmer } from "./Shimmer";
 
 const Body = () => {
-  const [listOfRest,setListOfRest]=useState(restList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
+
+    //Optional Chaining
+    setListOfRestaurant(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        Restaurant:
-        <button className="filter-btn" onClick={() => {
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="Search any Restaurant.."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-box-btn"
+            onClick={() => {
+              //Filter the restaurants and update the UI
+              const filteredRestaurant = listOfRestaurant.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        OR
+        <button
+          className="filter-btn"
+          onClick={() => {
             //Filter logic here
-            const filteredList=listOfRest.filter((res)=>res.data.avgRating>4);
-            setListOfRest(filteredList);
-            console.log(filteredList);
-        }}>
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+            // console.log(filteredList);
+          }}
+        >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRest.map((restaurant)=>(
-            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,18 +1,22 @@
-import {CDN_URL} from "../utils/mockdata";
+import { CDN_URL } from "../utils/mockdata";
 
 const RestaurantCard = (props) => {
-    const {resData}=props;
-    const {cloudinaryImageId,name,avgRating,cuisines,costForTwoString}=resData?.data;
+  const { resData } = props;
+  const { cloudinaryImageId, name, avgRating, cuisines, sla } = resData?.info;
 
-    return (
-      <div className="res-card">
-        <img src={CDN_URL + cloudinaryImageId} />
-        <h3><b><i>{name}</i></b></h3>
-        <h4>{cuisines.join(', ')}</h4>
-        <h4>{avgRating}</h4>
-        <h4>{costForTwoString}</h4>
-      </div>
-    );
-  };
+  return (
+    <div className="res-card">
+      <img src={CDN_URL + cloudinaryImageId} />
+      <h3>
+        <b>
+          <i>{name}</i>
+        </b>
+      </h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{sla.slaString}</h4>
+    </div>
+  );
+};
 
 export default RestaurantCard;
